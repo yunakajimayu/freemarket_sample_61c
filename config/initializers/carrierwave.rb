c454b876b3d3b0490305a03932925ea5ecfb2199
yunakajimayu <y_nakajima1002@icloud.com>
2020-01-27 20:47:18 +0900

    S3の設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,14 +9,14 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-    # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-    region: 'ap-northeast-1'
-  }
-
-  config.fog_directory  = 'freemarket61c'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket61c'
+      provider: 'AWS',
+      aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+      # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+      region: 'ap-northeast-1'
+    }
+    config.fog_directory  = 'freemarket61c'
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket61c'
+  end
 end
