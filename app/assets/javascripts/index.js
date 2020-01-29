7e411289fd99bdcd85d552263e6f38391246f9c7
soto-ogre <soto1oog@gmail.com>
2020-01-29 18:54:09 +0900

    header&footer banner

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,4 +6,23 @@ $(function(){
   });
 
   $('.animate').scrolla();
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  speed: 600,
+  autoplay: {delay: 4000},
+})
+  
 });
+
+
