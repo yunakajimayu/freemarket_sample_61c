5d33eebd87568e8a63990dc7a8f7abbe4df73107
soto-ogre <soto1oog@gmail.com>
2020-02-08 15:34:22 +0900

    front markup
    商品出品ページのフロントマークアップです。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,8 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def sell
+    render layout: 'sell'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
+  resource :items ,path:'//' do
+    get 'sell',on: :collection
+  end
   resource :user,path: '/mypage/' do
     get 'profile',on: :collection
   end
