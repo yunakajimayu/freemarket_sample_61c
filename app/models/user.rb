ffa0257ec48797af5696747728ac5299a759d955
indiamela <taishi.kusunose@gmail.com>
2020-02-08 18:51:10 +0900

    create sns_confarmation_action

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,21 @@ class User < ApplicationRecord
   has_one :profile
 
   has_many :sns_credentials
+
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    # sns認証したことがあればアソシエーションで取得
+    # 無ければemailでユーザー検索して取得orビルド(保存はしない)
+    user = sns.user || User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みの場合はそのままログインの処理へ行くので、ここでsnsのuser_idを更新しておく
+    if user.persisted?
+      sns.user = user
+      sns.save
+    end
+    user
+  end
+  
 end
