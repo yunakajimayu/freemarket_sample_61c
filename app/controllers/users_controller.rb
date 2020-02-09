4ebcd2c964f766068debeb1aef11c9ba457985bb
yunakajimayu <y_nakajima1002@icloud.com>
2020-02-09 17:14:54 +0900

    ユーザー本人確認ページのマークアップ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,8 @@ class UsersController < ApplicationController
   def profile
     
   end
+
+  def identification
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root 'items#index'
   resource :user,path: '/mypage/' do
     get 'profile',on: :collection
+    get 'identification',on: :collection
   end
 end
