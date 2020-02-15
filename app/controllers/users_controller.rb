d6eaa7565b7da89d84502dc2074097bfc38d39a6
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-02-15 15:43:06 +0900

    fix-conflict-2/15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+  layout false
 
   def show
     
   end
 
   def profile
-    
+    render layout: 'users'
   end
+  
+    
 
   def identification
 
