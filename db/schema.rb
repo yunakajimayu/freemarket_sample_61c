27df643c3ef180348bac18d31c93143dca60b4c4
akira9981 <56640109+akira9981@users.noreply.github.com>
2020-02-16 14:55:04 +0900

    complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_08_082244) do
+ActiveRecord::Schema.define(version: 2020_02_08_091331) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "first_name", default: "", null: false
