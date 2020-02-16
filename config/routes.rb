263223d11cc23ea75196f3ccfbf8e43f36afb5d3
yunakajimayu <y_nakajima1002@icloud.com>
2020-02-16 15:57:31 +0900

    routeの修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   end
   devise_for :users, path: '', controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   
