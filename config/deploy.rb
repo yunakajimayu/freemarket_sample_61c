7e8d8478aa1aa2b4a21cd9531aedc20b0a7f66b0
yunakajimayu <y_nakajima1002@icloud.com>
2020-03-18 21:45:59 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml.enc','.env'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
