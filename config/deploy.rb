78b3fb7e7a7a8d8e6a25513849b685fb0cb45cbb
yunakajimayu <y_nakajima1002@icloud.com>
2020-03-21 22:07:26 +0900

    bundlerのバージョン変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,7 +37,7 @@ namespace :deploy do
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc"),("env")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc","env")
     end
   end
   before :starting, 'deploy:upload'
