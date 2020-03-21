8db5fa1c16aab853a4594c9dcf9a52efebbd9217
yunakajimayu <y_nakajima1002@icloud.com>
2020-03-21 22:21:20 +0900

    capistranoのバージョン変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,13 +31,13 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 
-  desc 'upload credentials.yml.enc',"env"
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
         execute "mkdir -p #{shared_path}/config"
       end
-      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc","env")
+      upload!('config/credentials.yml.enc', "#{shared_path}/config/credentials.yml.enc")
     end
   end
   before :starting, 'deploy:upload'
